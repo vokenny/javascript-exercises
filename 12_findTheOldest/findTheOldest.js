0d3deee45dd47c9e048aec00334bc1854327b37b
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-10-13 21:32:59 +0100

    Complete find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+  if (Array.isArray(peopleArray)) {
+    const oldestPerson = peopleArray.reduce((oldest, person) => {
+      if (!oldest) return person;
 
+      const oldestAge = calcAge(oldest);
+      const personAge = calcAge(person);
+
+      return oldestAge >= personAge ? oldest : person;
+    });
+
+    return oldestPerson;
+  }
 };
+
+function calcAge(person) {
+  return !person.yearOfDeath
+    ? new Date().getFullYear() - person.yearOfBirth
+    : person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
