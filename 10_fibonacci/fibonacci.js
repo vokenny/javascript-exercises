ff237a8b8bf2569c5b70771a7dca61e50eaf9dd5
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-10-13 21:14:31 +0100

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (ordinal) {
+  const ordInt = parseInt(ordinal);
 
+  switch (true) {
+    case ordInt <= 0: return 'OOPS';
+    case ordInt < 3:
+      return 1;
+    default:
+      return fibonacci(ordInt - 1) + fibonacci(ordInt - 2);
+  }
 };
 
 // Do not edit below this line
