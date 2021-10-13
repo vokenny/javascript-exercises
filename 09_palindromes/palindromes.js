2df86e92b68c35ee9ca6cca046cb38c485d9da45
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-10-13 20:02:37 +0100

    Completed palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (query) {
+  if (typeof query != 'string') throw new Error(`Invalid input for palindrome check. Received: ${query}`);
 
+  const charArray = Array.from(
+    query
+      .trim()
+      .replace(/\s/g, '')
+      .toUpperCase()
+  );
+
+  const queryArray = charArray.filter(char => (/[A-Z]/).test(char));
+  const reversedArray = [...queryArray].reverse();
+
+  return queryArray.every((char, idx) => char === reversedArray[idx]);
 };
 
 // Do not edit below this line
